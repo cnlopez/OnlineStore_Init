cf866fda7f5b397d595e1b3e117d6ea24f2a892e
cnlopez <cnlopez1@hotmail.com>
2023-06-15 16:48:05 -0500

    Unit tests base product service

---
+++ ProductService/Tests/Base/ProductUnitTestSetup.cs
@@ -0,0 +1,47 @@
+﻿using Business.Services;
+using Data;
+using Data.Interfaces;
+using Data.Models;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Tests.Base
+{
+    public partial class ProductUnitTest
+    {
+        private const string JsonBasePath = "Json/";
+        public IEnumerable<Products> ProductsModel { get; set; }
+
+        public virtual void Init()
+        {
+            ProductRepositoryMock = new Mock<IProductRepository>();
+            ProductService = new ProductService(ProductRepositoryMock.Object, Mapper);
+        }
+
+        [SetUp]
+        public virtual void Setup()
+        {
+            Init();
+            PopulateDatabase();
+        }
+
+        public static T LoadFromJsonFile<T>(string route)
+        {
+            var path = Path.GetFullPath(route);
+            using var reader = new StreamReader(path);
+            var json = reader.ReadToEnd();
+            return JsonSerializer.Deserialize<T>(json);
+        }
+
+        private void PopulateDatabase()
+        {
+            ProductsModel = LoadFromJsonFile<IEnumerable<Products>>(JsonBasePath + "Product.json");
+        }
+    }
+}
