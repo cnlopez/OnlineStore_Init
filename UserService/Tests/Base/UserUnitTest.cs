68e5589d3db63920258d9f3d695d469a28cdb7c4
cnlopez <cnlopez1@hotmail.com>
2023-06-19 11:52:54 -0500

    User service tests

---
+++ UserService/Tests/Base/UserUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Business.Interfaces;
+using Business.Mappers;
+using Data.Interfaces;
+using Moq;
+
+namespace Tests.Base
+{
+    public abstract partial class UserUnitTest
+    {
+        protected IUserService UserService { get; set; }
+        protected Mock<IUserRepository> UserRepositoryMock { get; set; }
+        protected IMapper Mapper { get; set; }
+
+        protected UserUnitTest()
+        {
+            var profiles = new Profile[] { new UserProfile() };
+            var mappingConfig = new MapperConfiguration(x => x.AddProfiles(profiles));
+            Mapper = mappingConfig.CreateMapper();
+        }
+    }
+}
---
+++ UserService/Tests/GetUsersTest.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Base;
+
+namespace Tests
+{
+    public class GetUsersTest : UserUnitTest
+    {
+        [Test]
+        public async Task GetUsers_ReturnSuccess()
+        {
+            UserRepositoryMock.Setup(x => x.GetUsersAsync()).ReturnsAsync(UserModel);
+            var response = await UserService.GetUsers();
+            Assert.IsNotNull(response);
+        }
+    }
+}
