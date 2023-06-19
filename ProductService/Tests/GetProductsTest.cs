fd17487fd61c43641a9b265ca6430bf976acf23a
cnlopez <cnlopez1@hotmail.com>
2023-06-18 21:30:03 -0500

    Product test

---
+++ ProductService/Tests/GetProductsTest.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Base;
+
+namespace Tests
+{
+    public class GetProductsTest : ProductUnitTest
+    {
+        [Test]
+        public async Task GetProducts_ReturnSuccess()
+        {
+            ProductRepositoryMock.Setup(x=>x.GetProductsAsync()).ReturnsAsync(ProductsModel);
+            var response = await ProductService.GetProducts();
+            Assert.IsNotNull(response);
+        }
+    }
+}
