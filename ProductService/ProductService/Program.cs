dadba26855caf16982a4f6f9b5473670c971b9d3
cnlopez <cnlopez1@hotmail.com>
2023-12-10 10:53:57 -0500

    jwt

--- ProductService/ProductService/Program.cs
+++ ProductService/ProductService/Program.cs
@@ -1,7 +1,34 @@
 using Business.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Services.Ioc;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("appsettings.json");
+var secretKey = builder.Configuration.GetSection("Jwt").GetSection("Secret").Value;
+var keyBytes = Encoding.UTF8.GetBytes(secretKey);
+
+builder.Services.AddAuthentication(config => {
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            //IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("0lIkKwy6pmp9X/C/PtT5JZ8jxbRfj5Xn98GZHfid2mo=")),
+            IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = "your_issuer",
+            ValidAudience = "your_audience",
+            ValidAlgorithms = new[] { SecurityAlgorithms.HmacSha256 }
+        };
+    });
 
 // Add services to the container.
 
