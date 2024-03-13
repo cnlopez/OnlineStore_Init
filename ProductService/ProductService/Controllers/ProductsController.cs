8f95d28701cf4bed13c1e51c75bb1096e1aa7499
cnlopez <cnlopez1@hotmail.com>
2024-03-13 11:45:52 -0500

    commit test

--- ProductService/ProductService/Controllers/ProductsController.cs
+++ ProductService/ProductService/Controllers/ProductsController.cs
@@ -59,7 +59,7 @@ namespace ProductService.Controllers
         }
 
         /// <summary>
-        /// Delete Product
+        /// Delete Product //Test
         /// </summary>
         /// <returns></returns>
         [Route("products/{productId:int}"), HttpDelete]
